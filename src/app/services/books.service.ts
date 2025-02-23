1c6f688c589c831e00a0226f74dd812dddc4b878
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-23 15:04:23 -0300

    feat: create books service

---
+++ src/app/services/books.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  private readonly url = environment.apiUrl;
+  constructor(private readonly http: HttpClient) {}
+
+  getBooks(value: string): Observable<any> {
+    const params = new HttpParams().append('q', value);
+    return this.http.get(this.url, { params });
+  }
+}
