19eeb65bc2215ab8713aab648239a4d5237abecd
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-23 16:29:04 -0300

    feat: create modal books component

---
+++ src/app/components/books/modal-books/modal-books.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+const body = document.querySelector('body');
+@Component({
+  selector: 'app-modal-books',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal-books.component.html',
+  styleUrl: './modal-books.component.css',
+})
+export class ModalBooksComponent {
+  @Input() book: Object = '';
+  statusModal: boolean = false;
+  @Output() changedModal = new EventEmitter();
+  closeModal() {
+    this.statusModal = false;
+    this.changedModal.emit(this.statusModal);
+    body!.style.overflow = 'scroll';
+  }
+  hiddenModal() {
+    if (this.statusModal === true) {
+      body!.style.overflow = 'hidden';
+    }
+  }
+  readPreview() {
+    window.open('_blank');
+  }
+}
