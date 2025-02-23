98050db688beb5ac56a422caf3e0aaf4af1cb925
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-23 16:58:54 -0300

    refactor: return observable to result books

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { IResultBooks } from '../models/index';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,8 +10,8 @@ export class BooksService {
   private readonly url = environment.apiUrl;
   constructor(private readonly http: HttpClient) {}
 
-  getBooks(value: string): Observable<any> {
+  getBooks(value: string): Observable<IResultBooks> {
     const params = new HttpParams().append('q', value);
-    return this.http.get(this.url, { params });
+    return this.http.get<IResultBooks>(this.url, { params });
   }
 }
