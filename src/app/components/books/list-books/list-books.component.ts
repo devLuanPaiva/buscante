05c24e9658931083c8b2dfe7c74764632e1236da
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-02-24 07:41:55 -0300

    refactor: implements onDestroy

--- src/app/components/books/list-books/list-books.component.ts
+++ src/app/components/books/list-books/list-books.component.ts
@@ -1,27 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { BooksService } from '../../../services/books.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BookComponent } from '../book/book.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { IBook } from '../../../models';
+import { Subscription } from 'rxjs';
 @Component({
-    selector: 'app-list-books',
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, BookComponent, FontAwesomeModule],
-    templateUrl: './list-books.component.html',
-    styleUrl: './list-books.component.css'
+  selector: 'app-list-books',
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, BookComponent, FontAwesomeModule],
+  templateUrl: './list-books.component.html',
+  styleUrl: './list-books.component.css'
 })
-export class ListBooksComponent {
-  listBooks: [] | undefined;
+export class ListBooksComponent implements OnDestroy {
+  listBooks: IBook[] = [];
   inputSearch: string = '';
   faMagnifyingGlass = faMagnifyingGlass;
-  constructor(private readonly booksService: BooksService) {}
+  subscription!: Subscription;
+
+  constructor(private readonly booksService: BooksService) { }
 
   searchBooks() {
-    this.booksService.getBooks(this.inputSearch).subscribe({
+    this.subscription = this.booksService.getBooks(this.inputSearch).subscribe({
       next: (books) => (this.listBooks = books),
       error: (error) => console.error('Error fetching books:', error),
       complete: () => console.info('Books loaded!'),
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 }
