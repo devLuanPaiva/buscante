4114b4f2fb32e5b0dee2f91c880989c21e7557a6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-25 20:41:55 -0300

    fix: ident class

--- src/app/models/class/book-vol-info.ts
+++ src/app/models/class/book-vol-info.ts
@@ -10,12 +10,12 @@ export class BookVolInfo {
   thumbnail?: IImageLinks;
 
   constructor(item: any) {
-    (this.title = item.volumeInfo?.title),
-      (this.authors = item.volumeInfo?.authors),
-      (this.publisher = item.volumeInfo?.publisher),
-      (this.publishedDate = item.volumeInfo?.publishedDate),
-      (this.description = item.volumeInfo?.description),
-      (this.previewLink = item.volumeInfo?.previewLink),
-      (this.thumbnail = item.volumeInfo?.imageLinks?.thumbnail);
+    this.title = item.volumeInfo?.title;
+    this.authors = item.volumeInfo?.authors;
+    this.publisher = item.volumeInfo?.publisher;
+    this.publishedDate = item.volumeInfo?.publishedDate
+    this.description = item.volumeInfo?.description;
+    this.previewLink = item.volumeInfo?.previewLink;
+    this.thumbnail = item.volumeInfo?.imageLinks?.thumbnail
   }
 }
