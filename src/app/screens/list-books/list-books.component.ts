83819870ab37115d1ef3af14c9e23fd7bcf391d6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-25 20:42:53 -0300

    refactor: use new function

--- src/app/screens/list-books/list-books.component.ts
+++ src/app/screens/list-books/list-books.component.ts
@@ -3,28 +3,34 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { IBook } from '../../models';
+import { IBook, Item } from '../../models';
 import { Subscription } from 'rxjs';
 import { BookComponent } from '../../components/books/book/book.component';
 import { BooksService } from '../../services/books.service';
+import { BookVolInfo } from '../../models/class/book-vol-info';
 @Component({
   selector: 'app-list-books',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, BookComponent, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BookComponent,
+    FontAwesomeModule,
+  ],
   templateUrl: './list-books.component.html',
-  styleUrl: './list-books.component.css'
+  styleUrl: './list-books.component.css',
 })
 export class ListBooksComponent implements OnDestroy {
   listBooks: IBook[] = [];
   inputSearch: string = '';
   faMagnifyingGlass = faMagnifyingGlass;
   subscription?: Subscription;
-  book: IBook = {}
+  book: IBook = {};
 
-  constructor(private readonly booksService: BooksService) { }
+  constructor(private readonly booksService: BooksService) {}
 
   searchBooks(event: Event) {
-    event.preventDefault(); 
-    console.log('Chamou searchBooks com:', this.inputSearch);
+    event.preventDefault();
     this.subscription = this.booksService.getBooks(this.inputSearch).subscribe({
       next: (books) => (this.listBooks = this.fetchBooks(books)),
       error: (error) => console.error('Erro ao buscar livros:', error),
@@ -32,22 +38,12 @@ export class ListBooksComponent implements OnDestroy {
     });
   }
 
-  fetchBooks(items: any): IBook[] {
-    const books: IBook[] = [];
-    items.forEach((item: any) => {
-      books.push({
-        title: item.volumeInfo.title,
-        authors: item.volumeInfo.authors,
-        publisher: item.volumeInfo.publisher,
-        publishedDate: item.volumeInfo.publishedDate,
-        description: item.volumeInfo.description,
-        previewLink: item.volumeInfo.previewLink,
-        thumbnail: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : null,
-      });
-    })
-    return books;
+  fetchBooks(items: Item[]): BookVolInfo[] {
+    return items.map((item) => {
+      return new BookVolInfo(item);
+    });
   }
   ngOnDestroy() {
-    this.subscription?.unsubscribe()
+    this.subscription?.unsubscribe();
   }
 }
