8e0622e73ae8eaeeef4d48f348a318d14290eab3
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-02-27 07:42:04 -0300

    test: add test to display book title

---
+++ src/app/components/books/book/book.component.spec.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from "@angular/common";
+import { By } from "@angular/platform-browser";
+import { BookComponent } from "./book.component";
+import { mockBook } from "../../../mocks/books.mocks";
+import { AuthorsPipe } from "../../../pipes/authors.pipe";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ModalBooksComponent } from "../modal-books/modal-books.component";
+
+describe("BookComponent", () => {
+  let component: BookComponent;
+  let fixture: ComponentFixture<BookComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CommonModule, ModalBooksComponent, AuthorsPipe, BookComponent],
+    }).compileComponents();
+  });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookComponent);
+    component = fixture.componentInstance;
+    component.book = mockBook;
+    fixture.detectChanges();
+  });
+  it('should display book title', () => {
+    const titleElement = fixture.debugElement.query(By.css('.tittle'));
+    expect(titleElement.nativeElement.textContent).toContain(mockBook?.title?.slice(0, 30));
+  });
+
+})
