9e27522ea58c56304fdfcf6e02cad5ab167449b9
devLuanPaiva <devluanpaiva@gmail.com>
2025-02-28 13:44:28 -0300

    refactor: return empty array

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -14,13 +14,14 @@ export class BooksService {
     const params = new HttpParams().append('q', value);
     return this.http.get<IResultBooks>(this.url, { params }).pipe(
       tap(() => console.log('Requisição iniciada')),
-      map(result => result.items),
+      map(result => result.items ?? []),
       tap(items => console.log(`Livros encontrados: ${items.length}`)),
       catchError(error => {
         console.error('Erro ao buscar livros:', error);
-        throw error;
+        return [];
       }),
       finalize(() => console.log('Requisição finalizada'))
     );
   }
+
 }
