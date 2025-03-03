72a45c831746cbd14fdbedcc944e0f6dceaf6774
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-03 09:22:10 -0300

    feat: create slideInTrigger animation

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -6,3 +6,12 @@ export const fadeInTrigger = trigger('fadeIn', [
     animate('500ms ease-out', style({ opacity: 1 })),
   ]),
 ]);
+export const slideInTrigger = trigger('slideIn', [
+  transition(':enter', [
+    style({ transform: 'translateY(20px)', opacity: 0 }),
+    animate(
+      '300ms ease-out',
+      style({ transform: 'translateY(0)', opacity: 1 })
+    ),
+  ]),
+]);
