3892e25c493474b4ddccebdef56e401b1756669d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-03 09:47:50 -0300

    feat: use faseIn animation

--- src/app/components/books/book/book.component.ts
+++ src/app/components/books/book/book.component.ts
@@ -3,13 +3,15 @@ import { Component, Input } from '@angular/core';
 import { IBook } from '../../../models';
 import { ModalBooksComponent } from '../modal-books/modal-books.component';
 import { AuthorsPipe } from '../../../pipes/authors.pipe';
+import { fadeInTrigger } from '../../../animations';
 
 @Component({
   selector: 'app-book',
   standalone: true,
   imports: [CommonModule, ModalBooksComponent, AuthorsPipe],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  styleUrl: './book.component.css',
+  animations: [fadeInTrigger]
 })
 export class BookComponent {
   @Input() book!: IBook
