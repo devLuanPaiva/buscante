fd84d8ea94762e216c1dd1035da2b5695fae9856
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-04 19:59:34 -0300

    feat: implement generateBookPreview method

--- src/app/services/book-preview.service.ts
+++ src/app/services/book-preview.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { IBook } from '../models';
+import { environment } from '../../environments/environment';
+import { catchError, map, Observable, throwError, from } from 'rxjs';
+import {
+  GoogleGenerativeAI
+} from '@google/generative-ai';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookPreviewService {
+  private readonly apiKey = environment.gemini_api_key;
+  constructor() {}
 
-  constructor() { }
+  generateBookPreview(book: Partial<IBook>): Observable<string> {
+    if (!book.authors || !book.title) {
+      return throwError(() => new Error('Autor ou título do livro ausente.'));
+    }
+
+    const prompt = `Em português, crie com exatos 600 caracteres um preview interessante sobre o livro ${book.title}, dos autores ${book.authors}.`;
+
+    const genAI = new GoogleGenerativeAI(this.apiKey);
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
+    return from(
+      model.generateContent(prompt).then((response) => {
+        return response.response.text();
+      })
+    ).pipe(
+      map((preview) => preview.trim()),
+      catchError((error) => {
+        console.error('Erro ao gerar preview:', error);
+        return throwError(() => new Error('Falha ao gerar preview do livro.'));
+      })
+    );
+  }
 }
