08138e6b0f9761a7f6d8d0c2278a2f65168b857c
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-04 08:03:52 -0300

    feat: create book preview component screen

---
+++ src/app/screens/book-preview/book-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-preview',
+  imports: [],
+  templateUrl: './book-preview.component.html',
+  styleUrl: './book-preview.component.css'
+})
+export class BookPreviewComponent {
+
+}
