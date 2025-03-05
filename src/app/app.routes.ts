5749f6bcf690990ed02a1446e51160e01574e43b
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-05 07:36:15 -0300

    feat: add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./screens/list-books/list-books.module').then((m) => m.ListBooksModule)
+  },
+  {
+    path: '/preview',
+    loadChildren: () => import('./screens/book-preview/book-preview.component.module').then((m) => m.BookPreviewModule)
   }
 ];
