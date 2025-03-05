082f164ed912f266b35e90bfe8a41986205819bc
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-05 20:44:35 -0300

    feat: add animation

--- src/app/screens/book-preview/book-preview.component.ts
+++ src/app/screens/book-preview/book-preview.component.ts
@@ -1,26 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { BookPreviewService } from '../../services/book-preview.service';
 import { IBook } from '../../models';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-book-preview',
-  imports: [],
   templateUrl: './book-preview.component.html',
-  styleUrl: './book-preview.component.css'
+  styleUrls: ['./book-preview.component.css'],
+  animations: [
+    trigger('typing', [
+      state('hidden', style({ opacity: 0 })),
+      state('visible', style({ opacity: 1 })),
+      transition('hidden => visible', animate('0.5s ease-in')),
+    ]),
+  ],
 })
 export class BookPreviewComponent implements OnInit {
-  book!: Partial<IBook>
-  preview = ''
-  constructor(private readonly previewService: BookPreviewService) { }
+  book!: Partial<IBook>;
+  preview = '';
+  displayedText = '';
+  typingSpeed = 20; 
+  constructor(private readonly previewService: BookPreviewService) {}
 
   ngOnInit(): void {
     const storedBook = this.previewService.getBookFromSession();
     if (storedBook) {
       this.book = storedBook;
       this.previewService.generateBookPreview(this.book).subscribe({
-        next: (generatedPreview) => (this.preview = generatedPreview),
+        next: (generatedPreview) => this.startTypingEffect(generatedPreview),
         error: (err) => console.error('Erro ao gerar preview:', err),
       });
     }
+  }
+
+  startTypingEffect(text: string) {
+    this.preview = text;
+    this.displayedText = '';
+    let i = 0;
+
+    const interval = setInterval(() => {
+      if (i < this.preview.length) {
+        this.displayedText += this.preview[i];
+        i++;
+      } else {
+        clearInterval(interval);
+      }
+    }, this.typingSpeed);
   }
 }
