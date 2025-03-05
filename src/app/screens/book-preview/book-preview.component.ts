49bf67123aea63aa01f9786b08e26f3665bc15be
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-05 08:10:05 -0300

    feat: implement template

--- src/app/screens/book-preview/book-preview.component.ts
+++ src/app/screens/book-preview/book-preview.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 import { BookPreviewService } from '../../services/book-preview.service';
 import { IBook } from '../../models';
 
 @Component({
   selector: 'app-book-preview',
-  imports: [],
   templateUrl: './book-preview.component.html',
-  styleUrl: './book-preview.component.css'
+  styleUrl: './book-preview.component.css',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-10px)' }),
+        animate('500ms ease-in-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class BookPreviewComponent implements OnInit {
-  book!: Partial<IBook>
-  preview = ''
+  book!: Partial<IBook>;
+  preview = '';
+  animatedText = '';
+
   constructor(private readonly previewService: BookPreviewService) { }
 
   ngOnInit(): void {
@@ -18,9 +28,24 @@ export class BookPreviewComponent implements OnInit {
     if (storedBook) {
       this.book = storedBook;
       this.previewService.generateBookPreview(this.book).subscribe({
-        next: (generatedPreview) => (this.preview = generatedPreview),
+        next: (generatedPreview) => {
+          this.preview = generatedPreview;
+          this.animateText();
+        },
         error: (err) => console.error('Erro ao gerar preview:', err),
       });
     }
+  }
+
+  animateText() {
+    this.animatedText = '';
+    let i = 0;
+    const interval = setInterval(() => {
+      this.animatedText += this.preview[i];
+      i++;
+      if (i === this.preview.length) {
+        clearInterval(interval);
+      }
+    }, 30);
   }
 }
