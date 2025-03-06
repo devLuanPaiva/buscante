25f21dce3f389075567ed80a2e89e46e788e9897
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-06 08:05:39 -0300

    test: remove test file

--- src/app/services/book-preview.service.spec.ts
+++ src/app/services/book-preview.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BookPreviewService } from './book-preview.service';
 
 describe('BookPreviewService', () => {
@@ -8,9 +7,25 @@ describe('BookPreviewService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(BookPreviewService);
+
+    spyOn(sessionStorage, 'getItem').and.callFake((key: string) => {
+      if (key === 'bookPreview') {
+        return JSON.stringify({ title: 'Test Book', authors: ['Author'] });
+      }
+      return null;
+    });
+
+    spyOn(sessionStorage, 'setItem').and.callFake(() => { });
+    spyOn(sessionStorage, 'removeItem').and.callFake(() => { });
+    spyOn(sessionStorage, 'clear').and.callFake(() => { });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should throw an error if book has no title or authors', (done) => {
+    service.generateBookPreview({ title: 'Test Book' }).subscribe({
+      error: (err) => {
+        expect(err.message).toBe('Autor ou título do livro ausente.');
+        done();
+      },
+    });
   });
 });
