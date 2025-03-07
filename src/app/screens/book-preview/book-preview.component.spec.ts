dd1fe49db81e3a293518004e7a0f67bc7c877b56
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-07 07:30:57 -0300

    test: add test to retrieve book from session on init and generate preview

---
+++ src/app/screens/book-preview/book-preview.component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookPreviewComponent } from './book-preview.component';
+import { BookPreviewService } from '../../services/book-preview.service';
+import { of, throwError } from 'rxjs';
+import { IBook } from '../../models';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+describe('BookPreviewComponent', () => {
+  let component: BookPreviewComponent;
+  let fixture: ComponentFixture<BookPreviewComponent>;
+  let bookPreviewServiceSpy: jasmine.SpyObj<BookPreviewService>;
+
+  beforeEach(async () => {
+    const spy = jasmine.createSpyObj('BookPreviewService', ['getBookFromSession', 'generateBookPreview']);
+
+    await TestBed.configureTestingModule({
+      imports: [BookPreviewComponent],
+      providers: [{ provide: BookPreviewService, useValue: spy }, provideAnimations()],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BookPreviewComponent);
+    component = fixture.componentInstance;
+    bookPreviewServiceSpy = TestBed.inject(BookPreviewService) as jasmine.SpyObj<BookPreviewService>;
+  });
+  it('should retrieve book from session on init and generate preview', () => {
+    const mockBook: Partial<IBook> = { title: 'Test Book' };
+    const mockPreviewText = 'This is a preview of the book.';
+    bookPreviewServiceSpy.getBookFromSession.and.returnValue(of(mockBook));
+    bookPreviewServiceSpy.generateBookPreview.and.returnValue(of(mockPreviewText));
+
+    fixture.detectChanges();
+
+    expect(bookPreviewServiceSpy.getBookFromSession).toHaveBeenCalled();
+    expect(bookPreviewServiceSpy.generateBookPreview).toHaveBeenCalledWith(mockBook);
+    expect(component.book).toEqual(mockBook);
+  });
+
+})
