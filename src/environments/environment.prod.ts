2104a83e18e7d5c90c28e5774539da9d5f4fd0c6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-09 16:03:37 -0300

    refactor: delete environment files

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL:
-    typeof window !== 'undefined' ? (window as any).__env?.API_URL || '' : '',
-  GEMINI_API_KEY:
-    typeof window !== 'undefined'
-      ? (window as any).__env?.GEMINI_API_KEY || ''
-      : '',
-};
