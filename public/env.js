40b83ee137b2289489f450d66caa0c9be2b340b2
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-09 16:04:38 -0300

    chore: modify the script to generate both environment.ts and environment.prod.ts

--- public/env.js
+++
@@ -1,4 +0,0 @@
-window.__env = {
-    API_URL: "",
-    GEMINI_API_KEY: ""
-};
---
+++ scripts/env.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+
+const environmentsPath = path.join(__dirname, "../src/environments");
+
+if (!fs.existsSync(environmentsPath)) {
+    fs.mkdirSync(environmentsPath, { recursive: true });
+}
+
+const environmentFile = (isProd) => `
+export const environment = {
+  production: ${isProd},
+  API_URL: '${process.env.API_URL || ""}',
+  GEMINI_API_KEY: '${process.env.GEMINI_API_KEY || ""}'
+};
+`;
+
+fs.writeFileSync(path.join(environmentsPath, "environment.ts"), environmentFile(false));
+fs.writeFileSync(path.join(environmentsPath, "environment.prod.ts"), environmentFile(true));
+
