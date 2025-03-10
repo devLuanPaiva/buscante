2e79a6db276c03d87b09228b3915110d78f1bbd6
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-10 07:46:06 -0300

    feat: create pagination component

---
+++ src/app/components/books/pagination/pagination.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent {
+  @Input() totalItems = 0;
+  @Input() itemsPerPage = 10;
+  @Output() pageChange = new EventEmitter<number>();
+
+  currentPage = 1;
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.pageChange.emit(this.currentPage);
+    }
+  }
+}
