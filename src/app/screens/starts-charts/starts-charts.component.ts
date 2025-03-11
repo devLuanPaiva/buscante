a59e3227ea12b1ab1b702722ea3abc27350bbb93
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-11 20:45:05 -0300

    feat: implement service to display data from the most searched searches.

--- src/app/screens/starts-charts/starts-charts.component.ts
+++ src/app/screens/starts-charts/starts-charts.component.ts
@@ -6,24 +6,16 @@ import { StatsService } from '../../services/stats.service';
   selector: 'app-starts-charts',
   imports: [BrowserModule, NgxChartsModule],
   templateUrl: './starts-charts.component.html',
-  styleUrl: './starts-charts.component.css'
+  styleUrl: './starts-charts.component.css',
 })
 export class StartsChartsComponent implements OnInit {
-  searchData: string[] = []
-  categoryData: string[] = []
-  constructor(private readonly statsService: StatsService) { }
+  searchStats: { name: string; value: number }[] = [];
 
-  ngOnInit() {
-    this.updateCharts();
-  }
-  updateCharts() {
-    this.searchData = this.formatChartData(this.statsService.getSearchStats());
-    this.categoryData = this.formatChartData(this.statsService.getCategoryStats());
-  }
+  constructor(private readonly searchStatsService: StatsService) {}
 
-  private formatChartData(data: Record<string, number>): any[] {
-    return Object.entries(data).map(([name, value]) => ({ name, value }));
+  ngOnInit(): void {
+    this.searchStatsService.searchStats$.subscribe(
+      (stats) => (this.searchStats = stats)
+    );
   }
 }
-
-
