ee15807f426df3be05b3ec638dfc6f52c7d39340
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-12 07:41:35 -0300

    feat: implament save to local storage method

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StatsService {
   private searchCounts: { [query: string]: number } = {};
-  private readonly searchStatsSubject = new BehaviorSubject<
-    { name: string; value: number }[]
-  >([]);
-
+  private readonly searchStatsSubject = new BehaviorSubject<{ name: string; value: number }[]>([]);
   searchStats$ = this.searchStatsSubject.asObservable();
+
+  constructor() {
+    this.loadFromLocalStorage();
+  }
 
   registerSearch(query: string): void {
     if (query) {
       this.searchCounts[query] = (this.searchCounts[query] || 0) + 1;
       this.updateChartData();
+      this.saveToLocalStorage()
     }
   }
 
@@ -27,4 +29,18 @@ export class StatsService {
 
     this.searchStatsSubject.next(chartData);
   }
+
+  private saveToLocalStorage(): void {
+    if (typeof window === 'undefined') return;
+    localStorage.setItem('searchStats', JSON.stringify(this.searchCounts));
+  }
+
+
+  private loadFromLocalStorage(): void {
+    if (typeof window === 'undefined') return;
+    const savedData = localStorage.getItem('searchStats');
+    this.searchCounts = savedData ? JSON.parse(savedData) : {};
+    this.updateChartData();
+  }
+
 }
