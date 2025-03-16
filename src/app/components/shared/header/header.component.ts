60dde53ec1d9446c439fb2201b296616ebd44564
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-16 09:05:54 -0300

    refactor: remove goToHomeScreen method

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,18 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  constructor(private readonly router: Router) {}
-
-  goToHomeScreen() {
-    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-      this.router.navigateByUrl(this.router.url);
-    });
-  }
+  constructor() {}
 }
