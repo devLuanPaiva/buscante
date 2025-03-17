cefe5e223bfe1954c246dadfd782a0f03957cc42
devLuanPaiva <devluanpaiva@gmail.com>
2025-03-17 07:39:56 -0300

    test: add ActivatedRoute as a mocked provider

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: { get: () => null } },
+            queryParams: of({}),
+          },
+        },
+      ],
     }).compileComponents();
   });
 
